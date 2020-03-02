fb1959965fa4b38387741659177c16e877138af9
João Martins <up201707311@fe.up.pt>
2020-03-02 16:04:05 +0000

    added boilerplate

---
+++ components/footer.php
@@ -0,0 +1,11 @@
+<?php 
+    function make_footer() { ?>
+            <footer>
+                <div id="aboutus">About Us</div>
+                <div id="faqs">FAQs</div>
+                <div id="contacts">Contact Us</div>
+                <div id="copyright">Copyright &copy; Fytá</div>
+            </footer>
+        </body>
+    </html>
+<?php } ?>
---
+++ pages/homepage.php
@@ -0,0 +1,10 @@
+<?php
+    include_once('../components/footer.php');
+    include_once('../components/header.php');
+
+    make_header();
+    ?>
+    <h1>Fytá - Feel the flower power!</h1>
+    <?php 
+    make_footer();
+?>
