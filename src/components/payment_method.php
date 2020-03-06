5ebea82ab9aff2bf27b4a868ad21502758a08a93
João Martins <up201707311@fe.up.pt>
2020-03-06 17:16:34 +0000

    added requested changes

--- src/components/payment_method.php
+++ src/components/payment_method.php
@@ -4,21 +4,21 @@
             <h3>Payment Method</h3>
             <div class="row options">
                 <a href="#mb" class="col-6">
-                    <div class="img-wrapper border">
-                        <img src="../assets/mb.png" alt="mb" class="payment-img img-fluid">
+                    <div class="img-wrapper">
+                        <img src="../assets/mb.png" alt="mb" class="payment-img img-fluid border">
                     </div>
                     <div class="name w-100 text-center">Bank Transfer</div>
                 </a>
                 <a href="#stripe" class="col-6">
-                    <div class="img-wrapper border">
-                        <img src="../assets/stripe.png" alt="stripe" class="payment-img img-fluid">
+                    <div class="img-wrapper ">
+                        <img src="../assets/stripe.png" alt="stripe" class="payment-img img-fluid border">
                     </div>
                     <div class="name text-center">Stripe Payment</div>
                 </a>
             </div>
             <div class="row buttons lg-content-between sm-space-around">
-                <button type="button" id="back-btn" class="btn rounded-0 btn-lg">Back</button>
-                <button type="button" id="next-btn" class="btn rounded-0 btn-lg">Next</button>
+                <button type="button" id="back-btn" class="btn rounded-0 btn-lg shadow-none">Back</button>
+                <button type="button" id="next-btn" class="btn rounded-0 btn-lg shadow-none">Next</button>
             </div>
         </div>
 <?php } ?>
