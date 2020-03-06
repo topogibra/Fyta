278f2c73d3dcf2e17f8057a0d71b6ae33d94298c
João Martins <up201707311@fe.up.pt>
2020-03-06 22:07:29 +0000

    started order summary page

---
+++ src/components/order_summary.php
@@ -0,0 +1,88 @@
+<?php 
+    function order_summary() { ?>
+    <div class="order-summary container-fluid">
+        <div class="row summary-header">
+            <h3 class="col-6 text-nowrap">Order Summary</h3>
+            <div class="order-status col-6 row justify-content-center ">
+                <p class="font-weight-bold text-nowrap">Order Status:</p>
+                <p class="status font-weight-normal">Processed</p>
+            </div>
+        </div>
+        <div class="container summary">
+            <div class="row header">
+                <div class="col-md-5 col-sm-6 order-ref text-left">
+                    <p class="order-id font-weight-bold text-nowrap">Order #125877</p>
+                    <div class="order-placed row">
+                        <p class="text-nowrap">Placed on:</p>
+                        <p class="order-date font-weight-bold text-nowrap">Dec 24 2019</p> 
+
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 deliver-address">
+                    <p class="font-weight-bold text-nowrap">Deliver to:</p>
+                    <p class="order-recipient text-wrap">Ellie Black</p>
+                    <p class="order-address text-nowrap">Marcombe Dr NE, 334 3rd floor</p>
+                    <p class="order-location text-nowrap">Calgary, Canada</p>
+                </div>
+            </div>
+            <div class="row order-list-item">
+                <div class="col-md-3">
+                    <div class="img-wrapper">
+                        <img class="product-image img-fluid border border-dark" src="../assets/vaso.jpg" alt="Vaso">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <p class="product-name">Vaso CRTF</p>
+                </div>
+                <div class="col-md-3">
+                    <p class="product-price">20€</p>
+                </div>
+                <div class="col-md-3">
+                    <div class="quantity-box">1</div>
+                </div>
+            </div>
+            <div class="row order-list-item">
+                <div class="col-md-3">
+                    <div class="img-wrapper">
+                        <img class="product-image img-fluid border border-dark" src="../assets/vaso.jpg" alt="Vaso">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <p class="product-name ">Vaso CRTF</p>
+                </div>
+                <div class="col-md-3">
+                    <p class="product-price">20€</p>
+                </div>
+                <div class="col-md-3">
+                    <div class="quantity-box">1</div>
+                </div>
+            </div>
+            <div class="row order-list-item">
+                <div class="col-md-3">
+                    <div class="img-wrapper">
+                        <img class="product-image img-fluid border border-dark" src="../assets/vaso.jpg" alt="Vaso">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <p class="product-name">Vaso CRTF</p>
+                </div>
+                <div class="col-md-3">
+                    <p class="product-price">20€</p>
+                </div>
+                <div class="col-md-3">
+                    <div class="quantity-box">1</div>
+                </div>
+            </div>
+            <p class="delivery-fee">Delivery: FREE</p>
+            <div class="order-total">
+                <p class="total">Total Value:</p>
+                <p class="total-value">60€</p>
+            </div>
+        </div>
+        <div class="row buttons lg-content-between sm-space-around">
+                <button type="button" id="back-btn" class="btn rounded-0 btn-lg shadow-none">Back</button>
+                <button type="button" id="next-btn" class="btn rounded-0 btn-lg shadow-none">Finish</button>
+        </div>
+    
+    </div>
+<?php } ?>
--- src/pages/orderpage.php
+++ src/pages/orderpage.php
@@ -2,8 +2,10 @@
     include_once('../components/footer.php');
     include_once('../components/header.php');
     include_once('../components/payment_method.php');
+    include_once('../components/order_summary.php');
 
-    make_header([], ['../styles/orderpage.css','../styles/homepage.css']);
-    payment_method();
+    make_header([], ['../styles/order_summary.css','../styles/payment_method.css','../styles/homepage.css']);
+    //payment_method();
+    order_summary();
     make_footer();
 ?>
