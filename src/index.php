94d1d9774721ca6518bc830def4b4a8f8006b224
anamargaridarl <anamargaridarl@gmail.com>
2020-03-06 18:26:53 +0000

    rename pages for better merges

--- src/index.php
+++ src/index.php
@@ -1,3 +1,3 @@
 <?php
-    header('Location: pages/orderpage.php');
+    header('Location: pages/checkout.php');
 ?>
--- src/pages/checkout.php
+++ src/pages/checkout.php
@@ -3,7 +3,7 @@
     include_once('../components/header.php');
     include_once('../components/payment_method.php');
 
-    make_header([], ['../styles/orderpage.css','../styles/homepage.css']);
+    make_header([], ['../styles/checkout.css','../styles/homepage.css']);
     payment_method();
     make_footer();
 ?>
