428ce06730c3e4cc9663bc9d8a9ed98c6b677a27
anamargaridarl <anamargaridarl@gmail.com>
2020-03-07 13:04:12 +0000

    worked on responsive design

--- src/javascript/searchpage.js
+++ src/javascript/searchpage.js
@@ -3,16 +3,17 @@ let filter = document.getElementById('filter');
 let pricerange = document.querySelector(".price input");
 let pricevalue = document.querySelector(".price label");
 
-
-
-let filtercontent = document.getElementsByClassName("col-lg-2");
+let filtericon = document.querySelector("#filter i")
+let filtercontent = document.getElementsByClassName("col-lg-3");
 
 
 filter.addEventListener('click', (event) => {
 
-    if (filtercontent[0].style.display == "none")
+    if (filtercontent[0].style.display == "none") {
+        filtericon.className = "fas fa-chevron-up";
         filtercontent[0].style.display = "block";
-    else {
+    } else {
+        filtericon.className = "fas fa-chevron-down";
         filtercontent[0].style.display = "none";
     }
 })
