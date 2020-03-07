7d7df3ad8f73b1cee327f95a9193d52eceec666d
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2020-03-07 23:29:59 +0000

    Birthday inputs are now dropdown menus.

--- src/pages/registerpage.php
+++ src/pages/registerpage.php
@@ -7,21 +7,22 @@ make_header([], ["../styles/registerpage.css"]); ?>
   <form action="" class=".form-inline">
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <h1 class="text-center">Register</h1>
+        <h1 class="text-center form-title">Register</h1>
       </div>
     </div>
     <!--  -->
     <div class="row justify-content-center">
-      <img src="../assets/user.png" class="mx-auto d-block img-fluid rounded-circle" alt="User Image" id="registerUserImg">
+      <img src="../assets/user.png" class="mx-auto d-block img-fluid rounded-circle border border-dark rounded" alt="User Image" id="registerUserImg">
+
     </div>
     <!-- -->
     <div class="row form-group justify-content-center">
       <div class="col-sm-6">
-        <input type="text" name="registerUsername" id="registerUsername" class="form-control" placeholder="Username" aria-describedby="helpUser">
+        <input type="text" name="registerUsername" id="registerUsername" class="form-control registerinput" placeholder="Username" aria-describedby="helpUser">
         <label for="registerPassword"></label>
-        <input type="email" name="registerEmail" id="registerEmail" class="form-control" placeholder="Email" aria-describedby="helpId">
+        <input type="email" name="registerEmail" id="registerEmail" class="form-control registerinput" placeholder="Email" aria-describedby="helpId">
         <label for="registerPassword"></label>
-        <input type="text" name="registerAddress" id="registerAddress" class="form-control" placeholder="Address" aria-describedby="helpId">
+        <input type="text" name="registerAddress" id="registerAddress" class="form-control registerinput" placeholder="Address" aria-describedby="helpId">
       </div>
     </div>
     <!--  -->
@@ -31,27 +32,43 @@ make_header([], ["../styles/registerpage.css"]); ?>
       </div>
     </div>
     <!--  -->
-    <div class="row form-group justify-content-center">
-      <div class="col-sm-2">
-        <input type="text" name="registerDay" id="registerDay" class="form-control" placeholder="Day" aria-describedby="helpId">
+    <div class="row form-group justify-content-center registerBirthday">
+      <div class="col-md-2 ">
+        <select class="custom-select custom-select-sm registerinput registerSelect" name="registerDay" id="registerDay">
+          <option selected class="text-muted optionplaceholder" hidden>Day</option>
+          <option value="">1</option>
+          <option value="">2</option>
+          <option value="">3</option>
+        </select>
       </div>
-      <div class="col-sm-2">
-        <input type="text" name="registerMonth" id="registerMonth" class="form-control" placeholder="Month" aria-describedby="helpId">
+      <div class="col-md-2 ">
+        <select class="custom-select custom-select-sm registerinput registerSelect" name="registerMonth" id="registerMonth">
+          <option selected class="text-muted optionplaceholder" hidden>Month</option>
+          <option value="">January</option>
+          <option value="">February</option>
+          <option value="">December</option>
+        </select>
       </div>
-      <div class="col-sm-2">
-        <input type="text" name="registerYear" id="registerYear" class="form-control" placeholder="Year" aria-describedby="helpId">
+      <div class="col-md-2 ">
+        <select class="custom-select custom-select-sm registerinput registerSelect" name="registerYear" id="registerYear">
+          <option selected class="text-muted optionplaceholder" hidden>Year</option>
+          <option value="">1999</option>
+          <option value="">2000</option>
+          <option value="">2001</option>
+        </select>
       </div>
+
     </div>
     <!--  -->
     <div class="form-group row justify-content-center">
       <div class="col-sm-6">
-        <input type="password" name="registerPassword" id="registerPassword" class="form-control" placeholder="Password" aria-describedby="helpId">
+        <input type="password" name="registerPassword" id="registerPassword" class="form-control registerinput" placeholder="Password" aria-describedby="helpId">
       </div>
     </div>
     <!--  -->
     <div class="row justify-content-center">
       <div class="col-sm-6 ">
-        <button type="submit" class="btn btn-primary col-sm-12">Register</button>
+        <button type="submit" class="btn btn-primary col-sm-12" id="registerbutton">Register</button>
       </div>
     </div>
   </form>
