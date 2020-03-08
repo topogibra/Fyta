ffc52d2bc98fb86b5ac960905a71fd57f16fedf4
João Martins <up201707311@fe.up.pt>
2020-03-08 00:24:15 +0000

    added order invoice and modularized order summary

--- src/pages/checkout_order_summary.php
+++ src/pages/checkout_order_summary.php
@@ -5,6 +5,21 @@
     include_once('../components/order_summary.php');
 
     make_header([], ['../styles/order_summary.css','../styles/payment_method.css']);
-    order_summary();
+    ?>
+    <div class="order-summary container-fluid">
+        <div class="row summary-header">
+            <h3 class="col-6 text-nowrap">Order Summary</h3>
+            <div class="order-status col-6 row justify-content-center ">
+                <p class="font-weight-bold text-nowrap">Order Status:</p>
+                <p class="status font-weight-normal">Processed</p>
+            </div>
+        </div>
+<?php order_summary(); ?>
+        <div class="row buttons lg-content-between sm-space-around">
+                <button type="button" id="back-btn" class="btn rounded-0 btn-lg shadow-none">Back</button>
+                <button type="button" id="next-btn" class="btn rounded-0 btn-lg shadow-none">Finish</button>
+        </div>
+    </div>
+<?php    
     make_footer();
 ?>
