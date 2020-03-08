87d35ff128ab78fe3674969f411b280a720651cf
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2020-03-08 16:14:47 +0000

    Trying to do some responsiveness.

--- src/pages/shoppingcartpage.php
+++ src/pages/shoppingcartpage.php
@@ -2,38 +2,33 @@
 include_once('../components/footer.php');
 include_once('../components/header.php');
 
-
-
-
-
-
 make_header([], ["../styles/shoppingcartpage.css"]); ?>
-<div class="container-md ">
+<div class="container-xl ">
   <div class="row justify-content-center">
     <div class="col-sm">
       <h1 class="text-center">Shopping Cart</h1>
     </div>
   </div>
   <!--  -->
-  <div class="row justify-content-center border-bottom shopCartEntry">
-    <div class="col-sm-1 align-middle">
+  <div class="row row-cols-12 justify-content-center border-bottom shopCartEntry">
+    <div class="col-sm align-middle order-1">
       <img src="../assets/vaso.png" alt="" class="shopCartProduct-image">
     </div>
-    <div class="col-sm-3 align-self-center shopCartProduct-name">
+    <div class="col-sm align-self-center shopCartProduct-name order-2">
       <p class="mb-0">Vaso CRTF</p>
     </div>
-    <div class="col-1 align-self-center shopCartProduct-per-price">
+    <div class="col-sm align-self-center shopCartProduct-per-price order-3">
       <p class="mb-0 text-right">20€</p>
     </div>
-    <div class="col-sm-3 text-center align-self-center shopCartProduct-stock">
+    <div class="col-sm text-center align-self-center shopCartProduct-stock order-4">
       <a href="" class="stock-minus"><i class="fas fa-minus"></i></a>
       <span class="ml-2 mr-2">10</span>
       <a href="" class="stock-plus"><i class="fas fa-plus"></i></a>
     </div>
-    <div class="col-1 align-self-center">
+    <div class="col-sm align-self-center order-5">
       <a href="" class="shopCartProduct-trash"><i class=" fas fa-trash"></i></a>
     </div>
-    <div class="col-1 align-self-center shopCartProduct-total">
+    <div class="col-sm align-self-center shopCartProduct-total order-6">
       <p class="mb-0 text-right">199.90€</p>
     </div>
   </div>
@@ -62,10 +57,10 @@ make_header([], ["../styles/shoppingcartpage.css"]); ?>
   </div>
   <!--  -->
   <div class="row justify-content-center shopCart-total">
-    <div class="col-9 align-self-center text-right">
-      <h4 class="shopCart-totaltext" >Total Value:</h4>
+    <div class="col-sm-9 align-self-center text-right">
+      <h4 class="shopCart-totaltext">Total Value:</h4>
     </div>
-    <div class="col-1 align-self-center">
+    <div class="col-sm-1 align-self-center">
       <h5 class="text-center">400.00€</h5>
     </div>
   </div>
