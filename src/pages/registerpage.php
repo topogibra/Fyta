b019ceaf44e425954320abb77a9f1cd5cf3e8bd6
anamargaridarl <anamargaridarl@gmail.com>
2020-03-13 22:30:27 +0000

    continued improvements in pages

--- src/pages/registerpage.php
+++ src/pages/registerpage.php
@@ -3,21 +3,21 @@ include_once('../components/footer.php');
 include_once('../components/header.php');
 
 make_header([], ["../styles/registerpage.css"]); ?>
-<div class="container-xl">
-  <form action="" class=".form-inline">
-    <div class="row justify-content-center">
-      <div class="col-sm-6">
+<div class="container">
+  <form action="" class=".form form justify-content-center">
+    <div class="row ">
+      <div class="col">
         <h1 class="text-center form-title">Register</h1>
       </div>
     </div>
     <!--  -->
-    <div class="row justify-content-center">
+    <div class="row ">
       <img src="../assets/user.png" class="mx-auto d-block img-fluid rounded-circle border border-dark rounded" alt="User Image" id="user-img">
 
     </div>
     <!-- -->
-    <div class="row form-group justify-content-center">
-      <div class="col-sm-6">
+    <div class="row form-group ">
+      <div class="col">
         <input type="text" name="username" id="username" class="form-control registerinput" placeholder="Username" aria-describedby="helpUser">
         <label for="password"></label>
         <input type="email" name="email" id="email" class="form-control registerinput" placeholder="Email" aria-describedby="helpId">
@@ -27,13 +27,13 @@ make_header([], ["../styles/registerpage.css"]); ?>
     </div>
     <!--  -->
     <div class="row">
-      <div class="col-sm-6">
+      <div class="col">
         <h4 id="birthday">Birthday</h4>
       </div>
     </div>
     <!--  -->
-    <div class="row form-group justify-content-center birthday">
-      <div class="col-sm-2 ">
+    <div class="row form-group  birthday">
+      <div class="col ">
         <select class="custom-select custom-select-sm registerinput registerSelect" name="day" id="day">
           <option selected class="text-muted optionplaceholder" hidden>Day</option>
           <option value="">1</option>
@@ -41,7 +41,7 @@ make_header([], ["../styles/registerpage.css"]); ?>
           <option value="">3</option>
         </select>
       </div>
-      <div class="col-sm-2 ">
+      <div class="col ">
         <select class="custom-select custom-select-sm registerinput registerSelect" name="month" id="month">
           <option selected class="text-muted optionplaceholder" hidden>Month</option>
           <option value="">January</option>
@@ -49,7 +49,7 @@ make_header([], ["../styles/registerpage.css"]); ?>
           <option value="">December</option>
         </select>
       </div>
-      <div class="col-sm-2 ">
+      <div class="col ">
         <select class="custom-select custom-select-sm registerinput registerSelect" name="year" id="year">
           <option selected class="text-muted optionplaceholder" hidden>Year</option>
           <option value="">1999</option>
@@ -60,15 +60,15 @@ make_header([], ["../styles/registerpage.css"]); ?>
 
     </div>
     <!--  -->
-    <div class="form-group row justify-content-center">
-      <div class="col-sm-6">
+    <div class="form-group row ">
+      <div class="col">
         <input type="password" name="password" id="password" class="form-control registerinput" placeholder="Password" aria-describedby="helpId">
       </div>
     </div>
     <!--  -->
-    <div class="row justify-content-center">
-      <div class="col-sm-6 ">
-        <a type="submit" href="profile_page.php" class="btn btn-primary col-sm-12" id="submitbutton">Register</a>
+    <div class="row ">
+      <div class="col ">
+        <a type="submit" href="profile_page.php" class="btn rounded-0 btn-lg shadow-none" id="submitbutton">Register</a>
       </div>
     </div>
   </form>
