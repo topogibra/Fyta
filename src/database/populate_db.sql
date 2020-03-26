9cc8cb5331cb675289f608eeacf953578cbbdc03
João Martins <up201707311@fe.up.pt>
2020-03-26 17:40:50 +0000

    first additions to db

---
+++ src/database/populate_db.sql
@@ -0,0 +1,22 @@
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (43,70,10),(52,44,14),(1,38,17),(21,14,11),(90,68,15),(72,81,20),(24,35,19),(27,87,11),(24,8,7),(2,20,9);
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (87,6,9),(67,98,18),(21,12,12),(98,85,18),(98,49,20),(17,49,4),(25,13,8),(91,18,20),(29,36,14),(70,9,20);
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (25,35,12),(61,12,10),(96,60,5),(32,36,4),(61,96,1),(12,47,17),(27,55,7),(48,86,1),(26,88,9),(93,96,17);
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (19,55,8),(88,45,1),(63,1,1),(79,78,19),(73,84,6),(46,43,20),(50,93,10),(13,56,16),(22,24,18),(68,18,17);
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (20,65,3),(46,59,10),(95,3,16),(12,16,8),(65,17,10),(83,98,9),(54,87,9),(100,60,6),(59,37,17),(33,1,15);
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (17,95,8),(36,43,6),(44,18,6),(38,12,4),(80,25,15),(14,98,16),(48,82,8),(58,27,8),(20,50,1),(25,5,6);
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (57,68,11),(72,11,2),(75,51,20),(8,72,5),(47,72,3),(7,50,12),(41,33,1),(25,54,11),(87,1,10),(24,43,18);
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (39,75,2),(56,100,1),(2,16,11),(16,78,1),(46,14,3),(65,29,19),(5,97,10),(8,18,6),(45,36,11),(81,36,6);
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (77,56,8),(95,62,12),(38,83,6),(83,34,11),(90,30,1),(99,64,11),(8,52,16),(60,92,15),(79,98,15),(60,29,6);
+INSERT INTO customer_product (id_customer,id_product,quantity) VALUES (7,72,3),(94,19,1),(85,96,14),(73,57,10),(87,35,20),(14,48,18),(84,55,4),(40,66,17),(32,73,14),(35,93,11);
+
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (32,20),(25,32),(31,18),(65,21),(93,18),(45,26),(77,20),(1,5),(60,8),(59,14);
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (93,27),(15,24),(5,15),(55,21),(17,33),(24,4),(53,38),(34,3),(18,23),(23,31);
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (78,39),(64,3),(26,34),(65,2),(28,8),(89,22),(93,17),(96,38),(7,17),(21,4);
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (85,14),(17,29),(81,9),(46,16),(51,6),(84,40),(2,22),(71,14),(16,25),(19,34);
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (92,12),(85,17),(30,23),(81,31),(23,29),(55,25),(91,14),(93,21),(34,28),(11,18);
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (69,33),(14,8),(4,28),(87,20),(85,31),(21,16),(39,3),(50,2),(9,10),(5,31);
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (15,16),(2,17),(67,16),(53,20),(29,4),(96,10),(92,21),(52,32),(51,8),(86,6);
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (94,36),(27,4),(94,19),(22,40),(69,7),(45,5),(36,10),(57,11),(31,36),(39,16);
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (42,17),(30,36),(53,4),(42,28),(85,15),(27,27),(91,25),(90,16),(99,37),(86,31);
+INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (15,38),(75,3),(45,24),(60,24),(77,12),(48,28),(15,33),(69,34),(17,10),(20,15);
+
