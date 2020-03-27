82418c68f0fb6eb617867984bdf10a9e810533b7
João Martins <up201707311@fe.up.pt>
2020-03-27 17:50:47 +0000

    updated populate script

--- src/database/populate_db.sql
+++ src/database/populate_db.sql
@@ -11,16 +11,16 @@ INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (42,17),(30,36),(53
 INSERT INTO wishlist_product (id_product,id_wishlist) VALUES (15,38),(75,3),(45,24),(60,24),(77,12),(48,28),(15,33),(69,34),(17,10),(20,15);
 
 --R09
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (43,70,10),(52,44,14),(1,38,17),(21,14,11),(90,68,15),(72,81,20),(24,35,19),(27,87,11),(24,8,7),(2,20,9);
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (87,6,9),(67,98,18),(21,12,12),(98,85,18),(98,49,20),(17,49,4),(25,13,8),(91,18,20),(29,36,14),(70,9,20);
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (25,35,12),(61,12,10),(96,60,5),(32,36,4),(61,96,1),(12,47,17),(27,55,7),(48,86,1),(26,88,9),(93,96,17);
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (19,55,8),(88,45,1),(63,1,1),(79,78,19),(73,84,6),(46,43,20),(50,93,10),(13,56,16),(22,24,18),(68,18,17);
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (20,65,3),(46,59,10),(95,3,16),(12,16,8),(65,17,10),(83,98,9),(54,87,9),(100,60,6),(59,37,17),(33,1,15);
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (17,95,8),(36,43,6),(44,18,6),(38,12,4),(80,25,15),(14,98,16),(48,82,8),(58,27,8),(20,50,1),(25,5,6);
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (57,68,11),(72,11,2),(75,51,20),(8,72,5),(47,72,3),(7,50,12),(41,33,1),(25,54,11),(87,1,10),(24,43,18);
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (39,75,2),(56,100,1),(2,16,11),(16,78,1),(46,14,3),(65,29,19),(5,97,10),(8,18,6),(45,36,11),(81,36,6);
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (77,56,8),(95,62,12),(38,83,6),(83,34,11),(90,30,1),(99,64,11),(8,52,16),(60,92,15),(79,98,15),(60,29,6);
-INSERT INTO shopping_cart (id_customer,id_product,quantity) VALUES (7,72,3),(94,19,1),(85,96,14),(73,57,10),(87,35,20),(14,48,18),(84,55,4),(40,66,17),(32,73,14),(35,93,11);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (43,70,10),(52,44,14),(1,38,17),(21,14,11),(90,68,15),(72,81,20),(24,35,19),(27,87,11),(24,8,7),(2,20,9);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (87,6,9),(67,98,18),(21,12,12),(98,85,18),(98,49,20),(17,49,4),(25,13,8),(91,18,20),(29,36,14),(70,9,20);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (25,35,12),(61,12,10),(96,60,5),(32,36,4),(61,96,1),(12,47,17),(27,55,7),(48,86,1),(26,88,9),(93,96,17);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (19,55,8),(88,45,1),(63,1,1),(79,78,19),(73,84,6),(46,43,20),(50,93,10),(13,56,16),(22,24,18),(68,18,17);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (20,65,3),(46,59,10),(95,3,16),(12,16,8),(65,17,10),(83,98,9),(54,87,9),(100,60,6),(59,37,17),(33,1,15);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (17,95,8),(36,43,6),(44,18,6),(38,12,4),(80,25,15),(14,98,16),(48,82,8),(58,27,8),(20,50,1),(25,5,6);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (57,68,11),(72,11,2),(75,51,20),(8,72,5),(47,72,3),(7,50,12),(41,33,1),(25,54,11),(87,1,10),(24,43,18);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (39,75,2),(56,100,1),(2,16,11),(16,78,1),(46,14,3),(65,29,19),(5,97,10),(8,18,6),(45,36,11),(81,36,6);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (77,56,8),(95,62,12),(38,83,6),(83,34,11),(90,30,1),(99,64,11),(8,52,16),(60,92,15),(79,98,15),(60,29,6);
+INSERT INTO shopping_cart (id_user,id_product,quantity) VALUES (7,72,3),(94,19,1),(85,96,14),(73,57,10),(87,35,20),(14,48,18),(84,55,4),(40,66,17),(32,73,14),(35,93,11);
 
 --R10
 INSERT INTO discount (id,percentage,date_begin,date_end) VALUES (1,96,'2019-04-11','2019-12-21'),(2,88,'2020-02-25','2020-06-26'),(3,2,'2020-01-11','2020-02-02'),(4,88,'2019-08-03','2019-11-09');
@@ -65,8 +65,8 @@ INSERT INTO product_tag (id_tag,id_product) VALUES (2,26),(17,76),(16,100),(9,93
 INSERT INTO product_tag (id_tag,id_product) VALUES (20,89),(15,65),(15,51),(8,67),(4,70);
 
 --R15
-INSERT INTO ticket (id,ticket_type,id_customer) VALUES (1,'Faulty_Delivery',18),(2,'Product_Complaint',1),(3,'Faulty_Delivery',5),(4,'Faulty_Delivery',8);
-INSERT INTO ticket (id,ticket_type,id_customer) VALUES (5,'Faulty_Delivery',9),(6,'Payment_Error',8),(7,'Payment_Error',8),(8,'Payment_Error',11);
+INSERT INTO ticket (id,ticket_type,id_user) VALUES (1,'Faulty_Delivery',18),(2,'Product_Complaint',1),(3,'Faulty_Delivery',5),(4,'Faulty_Delivery',8);
+INSERT INTO ticket (id,ticket_type,id_user) VALUES (5,'Faulty_Delivery',9),(6,'Payment_Error',8),(7,'Payment_Error',8),(8,'Payment_Error',11);
 
 --R16
 INSERT INTO ticket_message (id,send_date,message,id_ticket,id_user) VALUES (1,'2019-06-27 06:48:10','I cought the delivery man throwing my package at the door. It was a bonsai. I want my money back. NOW.',4,4);
