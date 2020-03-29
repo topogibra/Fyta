a1103d4b52015bef24108268ad6859db4028be76
João Martins <up201707311@fe.up.pt>
2020-03-29 13:48:23 +0100

    attended to requested changes

--- src/database/queries.sql
+++ src/database/queries.sql
@@ -1,67 +1,82 @@
--- Product query  of clASs bASed on Tag (Homepage)
+-- Product query  of class bASed on Tag (Homepage)
 --SELECT01
 --nome,imagem,preco
-SELECT product."name",price,"path" AS image_path, "image"."description" AS image_alt,
-FROM product,tag,product_tag,product_image,"image"
-WHERE tag."name" = $tag_name AND product.id = product_tag.id_product AND
-    tag.id = product_tag.id_tag AND product_image.id_product = product.id
-    AND product_image.id_image = "image".id
+SELECT product."name", price
+FROM product, tag, product_tag
+WHERE tag."name" = $tag_name 
+    AND product.id = product_tag.id_product 
+    AND tag.id = product_tag.id_tag 
 ORDER BY views DESC LIMIT 4;
 
--- Product query bASed on id (Product page)
+-- Get all images from a product (General)
 --SELECT02
-SELECT "name",product."description" AS details,price,"path" AS image_path,"image"."description" AS image_alt
-FROM product, "image", product_image
-WHERE product.id = $product_id AND product_image.id_product = product.id 
+SELECT "path" AS product_image_path, "image"."description" AS image_alt,
+FROM product, product_image, "image"
+WHERE product.id = $id
+    AND product_image.id_product = product.id
     AND product_image.id_image = "image".id;
 
--- Reviews retrival query bASed on product id (Product page)
+-- Product query based on id (Product page)
 --SELECT03
-SELECT review."description" AS contents, rating, review_date, username, "path" AS image_path, "image"."description" AS image_alt 
+SELECT "name", product."description" AS details, price
+FROM product
+WHERE product.id = $product_id;
+
+-- Reviews retrival query based on product id (Product page)
+--SELECT04
+SELECT review."description" AS contents, rating, review_date, username, 
+    "path" AS image_path, "image"."description" AS image_alt 
 FROM review,product_order,"order","user","image"
-WHERE review.id_product = $product_id AND review.id_order = product_order.id_order
-    AND product_order.id_product = $product_id AND product_order.id_order = "order".id
-    AND "order".id_user = "user".id AND "user".id_image = "image".id
+WHERE review.id_product = $product_id 
+    AND review.id_order = product_order.id_order
+    AND product_order.id_product = $product_id 
+    AND product_order.id_order = "order".id
+    AND "order".id_user = "user".id 
+    AND "user".id_image = "image".id
 ORDER BY review_date DESC;
 
 -- Tags retrieve query (Search page)
---SELECT04
+--SELECT05
 SELECT "name" AS tag
 FROM tag
 
--- Products retrive query bASed on tag (Search page)
---SELECT05
-SELECT product."name" AS product_name , price, "path" AS image_path, "image".description AS image_alt
-FROM product,tag,product_tag,product_image,"image"
-WHERE tag.id = $tag_id AND product_tag.id_tag = $tag_id
-    AND product_tag.id_product = product.id AND product_image.id_product = product.id
-    AND product_image.id_image = image.id
+-- Products retrive query based on tag (Search page)
+--SELECT06
+SELECT product."name" AS product_name, price
+FROM product, tag, product_tag
+WHERE tag.id = $tag_id 
+    AND product_tag.id_tag = $tag_id
+    AND product_tag.id_product = product.id 
 ORDER BY product_name;
 
 -- Customer information retrieval query (Profile page)
---SELECT06
-SELECT username, email, "date", "path" AS image_path, "description" AS image_alt
+--SELECT07
+SELECT username, email, "date", 
+    "path" AS image_path, "description" AS image_alt
 FROM "user","image"
-WHERE email = $email AND password_hash = $password_hash 
-	AND "user".user_role = 'Customer' AND "user".id_image = "image".id;
+WHERE email = $email 
+    AND password_hash = $password_hash 
+    AND "user".user_role = 'Customer' 
+    AND "user".id_image = "image".id;
 
 -- Order History collection bASed on user id (Order History page)
---SELECT07
+--SELECT08
 SELECT shipping_id, order_status, order_date
 FROM "order",order_history
-WHERE "order".id_user = $user_id AND order_history.id_order = "order".id
+WHERE "order".id_user = $user_id 
+    AND order_history.id_order = "order".id
 ORDER BY order_date DESC;
 
--- Wishilist AND size retrieval bASed on user id(Wishlist page)
---SELECT08
+-- Wishilist and size retrieval bASed on user id(Wishlist page)
+--SELECT09
 SELECT wishlist."name", COUNT(id_product) AS size
 FROM wishlist, wishlist_product
 WHERE wishlist.id_user = $user_id 
-AND wishlist_product.id_wishlist = wishlist.id
+    AND wishlist_product.id_wishlist = wishlist.id
 GROUP BY wishlist."name"
 
---Shopping cart bASed information bASed on user id(shopping cart page)
---SELECT09
+--Shopping cart based information based on user id(shopping cart page)
+--SELECT11
 SELECT product.name AS product_name, "image"."path" AS image_path, product.price AS product_price, quantity
 FROM shopping_cart, product, "image", product_image
 WHERE shopping_cart.id_user = $id AND 
@@ -70,7 +85,7 @@ WHERE shopping_cart.id_user = $id AND
       product.id = shopping_cart.id_product;
 
 -- Product query for order summary
---SELECT10.1
+--SELECT11
 SELECT "name",price, "path" AS image_path, quantity
 FROM product, product_image,"image",product_order
 WHERE product.id = product_order.id_product 
@@ -79,7 +94,7 @@ WHERE product.id = product_order.id_product
       AND product_image.id_image = "image".id;
 
 -- Product query for order summary
---SELECT10.2
+--SELECT12
 SELECT "order".delivery_address AS address, "order".order_date AS placed_at, username, order_status
 FROM "user", "order", order_history
 WHERE "order".id = $id
@@ -87,34 +102,26 @@ WHERE "order".id = $id
       AND order_history.id_order = $id
 
 --Retrieve information for manager 
---SELECT11
+--SELECT13
 SELECT username,email
 FROM "user"
 WHERE "user".user_role = 'Manager' AND "user".id = 41 ;
 
 
 --Retrieve stock information about products
---SELECT12
+--SELECT14
 SELECT "name" AS product_name, price, stock
 FROM product;
 
 --Retrieve pending orders
---SELECT13
+--SELECT15
 SELECT shipping_id,order_date,order_status
 FROM "order", order_history
 WHERE "order".id = order_history.id_order
 	  AND (order_status = 'Ready_for_Shipping' OR order_status = 'Awaiting_Payment');
 
 --Retrieve information FROM all the managers
---SELECT14
+--SELECT16
 SELECT "image"."path" AS image_path, username, "date" AS created_at
-<<<<<<< HEAD
 FROM "image", "user"
-WHERE "image".id = "user".id_image;
-
-
-
-=======
-FROM "image", user
-WHERE "image".id = user.id_image
->>>>>>> fc8e81b890c0cb374f7f31a291fc4a7e7702f8c3
+WHERE "image".id = "user".id_image AND "user".user_role = 'Manager'
