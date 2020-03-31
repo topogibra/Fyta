c978d96a5664da9365a77aecb1c78dc5e7082016
anamargaridarl <anamargaridarl@gmail.com>
2020-03-31 15:38:05 +0100

    grouped manager and costumer register

--- src/database/transactions/register_user.sql
+++ src/database/transactions/register_user.sql
@@ -8,7 +8,7 @@ WITH img_ins AS (
   RETURNING id AS img_id
 )
 -- Insert User
-INSERT INTO user (username,email,password_hash,"date","address",id_image,"Customer")
+INSERT INTO user (username,email,password_hash,"date","address",id_image,$type)
 VALUES ($username,$email,$password_hash,$date,$address,img_id,user_role)
 SELECT img_id FROM img_ins;
 
