12ca4414101f4893a1f397697e5cc8f41c847ffa
João Martins <up201707311@fe.up.pt>
2020-04-17 14:49:05 +0100

    general tweaks

--- resources/views/components/related_product.blade.php
+++ resources/views/components/related_product.blade.php
@@ -1,6 +1,6 @@
 <article class="col-md-4 col-sm-12">
     <div class="card align-items-center">
-        <img class="card-img-top border border-dark" src={{asset($item['img'])}} alt={{$item['name']}}>
+        <img href="/profile/{{ $item['id'] }}"class="border border-dark" src={{asset($item['img'])}} alt={{$item['name']}}>
         <div class="card-body row">
             <h4 class="card-title col">{{$item['name']}}</h4>
             <p class="card-text">{{$item['price']}}</p>
