cf92de21ed10f4d6d5cc42f76ecf419a3645f45a
João Martins <up201707311@fe.up.pt>
2020-04-18 18:50:52 +0100

    added verifcation of identity in invoice and profile

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -5,12 +5,25 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\Order;
-
+use Illuminate\Support\Facades\Auth;
 
 class InvoiceController extends Controller{
     
     public function invoice($id)
     {
+        if(!Auth::check()) {
+            abort(401);
+        }
+
+        $order = Order::find($id);
+        if(!$order) {
+            abort(400);
+        }
+        $user_id = Auth::id();
+        if($order->id_user != $user_id) {
+            abort(403);
+        }
+
         $products = Product::getOrderProducts($id);
         $information = Order::getOrderInformation($id);
         $status = Order::getOrderStatus($id);
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,8 +7,16 @@ use DateTime;
 use Illuminate\Support\Facades\Date;
 use App\Product;
 use App\Order;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class ProfileController extends Controller{
+    const CUSTOMER = 'Customer';
+    const MANAGER = 'Manager';
+    const GUEST = 'Guest';
+
 
     public function manager(){
         return view('pages.profile', ['layout' => ['scripts' => ['js/manager_page.js'], 'styles' => ['css/profile_page.css', 'css/registerpage.css']]]);
@@ -19,8 +27,14 @@ class ProfileController extends Controller{
     }
 
     public function orders() {
-        $user_id = 17; //TODO: get authenticated user
-        $user = User::find($user_id);
+        $role = $this->checkUser();
+        if($role == self::GUEST) {
+            return response()->json(['message' => 'You must login to access your order history'], 401);
+        }
+        else if($role == self::MANAGER)
+            return response()->json(['message' => 'Managers can\'t access order history'], 403);
+        
+        $user = Auth::user();
         $orders = $user->orders()->get()->all();
         $clean_orders = array_map(function($order) {
             $data = ['number'=> $order->shipping_id, 'date' => $order->order_date,'id' => $order->id];
@@ -36,18 +50,14 @@ class ProfileController extends Controller{
         return $clean_orders;
     }
 
-    public static function information() {
-        $managers = User::getManagersInfo()->all();
-        $clean_managers = array_map(function($manager) {
-            $data = ['name' => $manager->username, 'date' => $manager->date];
-            $data['photo'] = 'img/' . $manager->img_name;
-            return $data;
-        }, $managers);
-
-        return $clean_managers;
-    }
-
-    public static function stocks() {
+    public function stocks() {
+        $role = $this->checkUser();
+        if($role == self::GUEST) {
+            return response()->json(['message' => 'You must login to access stocks section'], 401);
+        }
+        else if($role == self::CUSTOMER)
+            return response()->json(['message' => 'You do not have access to this section'], 403);
+        
         $products = Product::getStockProducts()->all();
         $clean_products = array_map(function($product) {
             $data = ['name'=> $product->name, 'price' => $product->price, 'stock' => $product->stock ];
@@ -57,12 +67,19 @@ class ProfileController extends Controller{
         return $clean_products;
     }
 
-    public static function pending() {
+    public function pending() {
+        $role = $this->checkUser();
+        if($role == self::GUEST) {
+            return response()->json(['message' => 'You must login to access the pending orders'], 401);
+        }
+        else if($role == self::CUSTOMER)
+            return response()->json(['message' => 'You do not have access to this section'], 403);
+        
         $allstatus = Order::getStatusOrders()->all();
         $clean_status = array_map(function($status) {
             $data = ['number' => $status->shipping_id, 'date' => $status->order_date];
             $order_status = $status->order_status;
-            $order_status  =preg_replace("(_)"," ",$order_status);
+            $order_status  = preg_replace("(_)"," ",$order_status);
             $data['status'] = $order_status;
             $order_status;
             return $data;
@@ -71,9 +88,15 @@ class ProfileController extends Controller{
         return $clean_status;
     }
 
-    public static function managers() {
-        $user_id = 31; //TODO: get authenticated user
-        $user = User::find($user_id);
+    public function managers() {
+        $role = $this->checkUser();
+        if($role == self::GUEST) {
+          return response()->json(['message' => 'You must login to access the pending orders'], 401);
+        }
+        else if($role == self::CUSTOMER)
+          return response()->json(['message' => 'You do not have access to this section'], 403);
+        
+        $user = Auth::user();
         $managers = $user->getManagersInfo()->all();
         $clean_managers = array_map(function($manager) {
             $data = ['name' => $manager->username, 'date' => $manager->date];
@@ -85,8 +108,14 @@ class ProfileController extends Controller{
     }
     
     public function wishlist() {
-        $user_id = 17; //TODO: get authenticated user
-        $user = User::find($user_id);
+        $role = $this->checkUser();
+        if($role == self::GUEST) {
+            return response()->json(['message' => 'You must login to access your wishlist'], 401);
+        }
+        else if($role == self::MANAGER)
+            return response()->json(['message' => 'Managers can\'t access wishlist'], 403);
+        
+        $user = Auth::user();
         $wishlist = $user->wishlists()->first(); //TODO: integrate multiple wishlists
         if($wishlist == null) {
             return [];
@@ -102,10 +131,19 @@ class ProfileController extends Controller{
         return $items;
     }
 
-    public function profile()
+    public function profile(Request $request)
     {
-      $user_id = 17; //TODO: get authenticated user
-      $user = User::find($user_id);
+      $role = $this->checkUser();
+      if($role == self::GUEST)
+        return response()->json(['message' => 'You must login to access your profile'], 401);
+
+      if($request->path() == 'manager/get' && $role != self::MANAGER)
+        return response()->json(['message' => 'You do not have access to this section'], 403);
+      
+      if($request->path() == 'profile/get' && $role != self::CUSTOMER)
+      return response()->json(['message' => 'Managers can\'t access customer profile'], 403);
+            
+      $user = Auth::user();
       $img = $user->image()->first();
       $photo = 'img/' . $img->img_name;
       if($user->user_role == 'Customer')
@@ -123,4 +161,17 @@ class ProfileController extends Controller{
 
       return $data;
     }
+
+    public function checkUser()
+    {
+        if(!Auth::check())
+            return self::GUEST;
+
+        $user = Auth::user();
+        if($user->user_role != 'Customer')
+            return self::MANAGER;
+        else
+            return self::CUSTOMER;
+    }
+
 }
