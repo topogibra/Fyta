96e70764c72e5fe528cf97320af6eb16a67a70d4
João Martins <up201707311@fe.up.pt>
2020-04-19 16:05:59 +0100

    changed nav bar to deal with authenticated user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,16 +26,34 @@
                 <a class="navbar-brand" href="/home">
                     <img src={{ asset("img/logo.png") }} alt="Company Logo">
                 </a>
+                
                 <nav class="navbar navbar-expand-lg navbar-light navbar-icons">
-                    <a href="/profile" class="user">
-                        <i class="far fa-user"></i>
+                @auth  
+                    @if (User::checkUser() === User::$CUSTOMER)
+                        <a href="/profile" class="user">
+                            <i class="far fa-user"></i>
+                        </a>
+                        <a href="/profile#wishlist" class="star">
+                            <i class="far fa-star"></i>
+                        </a>
+                        <a href="/cart" class="cart">
+                            <i class="fas fa-shopping-basket"></i>
+                        </a>
+                    
+                    @else
+                        <a href="/manager" class="user">
+                            <i class="far fa-user"></i>
+                        </a>
+                        <a href="/product/add" class="star">
+                            <i class="far fa-plus-square"></i>
+                        </a>
+                    @endif      
+                @endauth
+                @guest
+                    <a href="/login" class="login">
+                        <i class="fas fa-user"></i>
                     </a>
-                    <a href="/profile#wishlist" class="star">
-                        <i class="far fa-star"></i>
-                    </a>
-                    <a href="/cart" class="cart">
-                        <i class="fas fa-shopping-basket"></i>
-                    </a>
+                    @endguest
                 </nav>
                 <nav class="navbar navbar-expand-md navbar-light navbar-categories">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#categoriesNavbar" aria-controls="categoriesNavbar" aria-expanded="false" aria-label="Toggle navigation">
