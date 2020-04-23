a46b50e19a5d63a8e9e4153c29cc090af50dd56e
anamargaridarl <anamargaridarl@gmail.com>
2020-04-23 17:21:18 +0100

    added fieldsets where necessary

--- resources/views/pages/order_summary.blade.php
+++ resources/views/pages/order_summary.blade.php
@@ -6,9 +6,9 @@
             <h3>Order Details</h3>
         <form action="/checkout-details" method="POST">
             @csrf
-
+            <fieldset>
             <div class="form-group">
-                <label for="deliveryaddress"> </label>
+                <label for="deliveryaddress"> Delivery Address </label>
                 <input type="text" class="form-control" id="deliveryaddress"  name="delivery" value={!! $address !!} placeholder="Delivery Address">
             </div>
             <h6>Billing Address </h6>
@@ -19,13 +19,13 @@
                     <label class="form-check-label"  for="checkout">Same Billing Address </label>
                 </div>
             </div>
-
+            
 
             <div class="form-group">
                 <label for="billingaddress"> </label>
                 <input type="text" class="form-control" id="billingaddress" name="billing" placeholder="Billing Address">
             </div>
-
+            <fieldset>
             @include('components.errors')
 
             <div class="d-flex flex-row-reverse">
