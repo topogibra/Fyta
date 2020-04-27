a726afd1a3d26e46fdb0fdc4946ef854e79dfcc0
anamargaridarl <anamargaridarl@gmail.com>
2020-04-27 14:52:11 +0100

    finished shopping cart

--- public/js/shopping_cart.js
+++ public/js/shopping_cart.js
@@ -1,45 +1,47 @@
 import { deleteData, postData } from './request.js'
 
 
-let garbage = document.querySelector('.shopCartProduct-trash');
-let less = document.querySelector('.stock-minus');
-let more = document.querySelector('.stock-plus');
+let garbage = document.querySelectorAll('.shopCartProduct-trash');
+let less = document.querySelectorAll('.stock-minus');
+let more = document.querySelectorAll('.stock-plus');
 
-garbage.addEventListener('click', async(event) => {
+for (let i = 0; i < garbage.length; i++) {
+    garbage[i].addEventListener('click', async(event) => {
 
-    event.preventDefault();
-    let response = await deleteData(garbage.href);
+        event.preventDefault();
+        let response = await deleteData(garbage[i].href);
 
-    if (response.status != 200)
-        return false;
-    else
-        location.reload();
-
-
-});
-
-more.addEventListener('click', async(event) => {
-
-    event.preventDefault();
-    let response = await postData(less.href, 1);
-
-    if (response.status != 200)
-        return false;
-    else
-        location.reload();
+        if (response.status != 200)
+            return false;
+        else
+            location.reload();
 
 
-});
+    });
 
-less.addEventListener('click', async(event) => {
+    more[i].addEventListener('click', async(event) => {
 
-    event.preventDefault();
-    let response = await postData(less.href, -1);
+        event.preventDefault();
+        let response = await postData(more[i].href, 1);
 
-    if (response.status != 200)
-        return false;
-    else
-        location.reload();
+        if (response.status != 200)
+            return false;
+        else
+            location.reload();
 
 
-});
+    });
+
+    less[i].addEventListener('click', async(event) => {
+
+        event.preventDefault();
+        let response = await postData(less[i].href, -1);
+
+        if (response.status != 200)
+            return false;
+        else
+            location.reload();
+
+
+    });
+}
