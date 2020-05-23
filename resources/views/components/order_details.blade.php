a0557060baec266390b6d774bc32df5737634071
Ana <anamargaridarl@gmail.com>
2020-05-23 13:07:55 +0100

    start responsivity in order_summary
    finished solving problems in order_summary

--- resources/views/components/order_details.blade.php
+++ resources/views/components/order_details.blade.php
@@ -1,29 +1,30 @@
 <div class="order-summary ">
     <div class="row summary-header">
-        <h3 class="col-6 text-nowrap">Order Summary</h3>
-        <div class="order-status col-6 row justify-content-center ">
-            <p class="font-weight-bold text-nowrap">Order Status:</p>
-            <p class="status font-weight-normal">{{$status->status}}</p>
-        </div>
+        <h3 class="col title text-nowrap">Order Summary</h3>
     </div>
     <div class="container summary">
         <div class="row header justify-space-around">
-            <div class="col-md-5 col-sm-6 order-ref text-left">
+            <div class="col-md-8  order-ref text-left">
                 <p class="order-id font-weight-bold text-nowrap">Order #{{$information->shipping_id}}</p>
                 <div class="order-placed row">
                     <p class="text-nowrap">Placed on:</p>
                     <p class="order-date font-weight-bold text-nowrap">{{$information->date}}</p>
-
+                </div>
+                <div class="order-status row">
+                <p class="font-weight-bold text-nowrap">Order Status:</p>
+                    <p class="status font-weight-normal text-nowrap">{{$status->status}}</p>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-6 deliver-address">
+            <div class="col-md-4  deliver-address ">
                 <p class="font-weight-bold ">Deliver to:</p>
                 <p class="order-recipient">{{$information->name}}</p>
                 <p class="order-address">{{$information->address}}</p>
                 <p class="order-location ">{{$location}}</p>
             </div>
         </div>
+        <div class="products-list">
         @each('components.order_item', $items, 'item')
+        </div>
         <div class="delivery-fee row justify-content-end">
             <p class="text-right">Delivery:</p>
             <p class="fee text-right">{{$delivery}}</p>
