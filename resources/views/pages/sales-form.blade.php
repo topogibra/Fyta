7991c047b5b0aaebc64e1eea62bb0a787f3144c4
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2020-06-02 15:26:40 +0100

    Added categories bar

--- resources/views/pages/sales-form.blade.php
+++ resources/views/pages/sales-form.blade.php
@@ -1,10 +1,11 @@
-@extends('layouts.product', ['content' => 'components.sales-form', 'method' => $method, 'files' =>  ['scripts' => ['js/sales_form.js'], 'styles' => ['css/product_page.css','css/sale_page.css','css/styles.css']]])
+@extends('layouts.product', ['content' => 'components.sales-form', 'method' => $method, 'files' =>  ['scripts' => ['js/sales_form.js'], 'styles' => ['css/product_page.css','css/styles.css','css/searchpage.css','css/sale_page.css']]])
 
 @section('sales-page-content')
-<div class="container mx-auto" id="form-content">
+<div class="container justify-content-center" id="form-content">
     <h3>Sale</h3>
+
     <input type="hidden" name="sale-id" id="sale-id" value={{ $id ?? -1 }}>
-    <div class="row number-input">
+    <div class="row">
         <div class="col">
             <div class="col">
                 <label for="begin">Begins at</label>
@@ -13,7 +14,7 @@
                 <input class="col" class="mt-1 d-block" name="begin" id="begin" type="date" value="{{ $begin ?? null}}" required>
             </div>
         </div>
-        <div class="col ">
+        <div class="col">
             <div class="col">
                 <label for="end">Ends at</label>
             </div>
@@ -21,32 +22,50 @@
                 <input class="col" class="mt-1 d-block" name="end" id="end" type="date" value="{{ $end ?? null}}" required>
             </div>
         </div>
-    </div>
-
-    <div class="row number-input">
-        <div class=" col">
+        <div class="col">
             <div class="col">
-                    <label for="percentage">Percentage (%)</label>
+                <label for="percentage">Percentage (%)</label>
             </div>
             <div class="col">
                 <input class="col" class="mt-1 d-block" name="percentage" id="percentage" type="number" step="1" value={{ $percentage ?? 1}} min="1" max="99" required>
             </div>
         </div>
     </div>
+    <div class="row">
+        <fieldset id="products-sales" name="apply-products" class="col">
+            <h5 class="text-center" id="legend">Select a date range to view eligible products</h5>
+        </fieldset>
+
+    </div>
+    <div class="row searchbuttons">
+        @include('components.filterButton')
+    </div>
     <div class="row" id="selProducts">
-        <div class="my-2 col ">
-            <fieldset id="products-sales" name="apply-products" class="col">
-                <legend>Select a date range to view eligible products</legend>
-            </fieldset>
-                    <div id="after-dates">
-                        <input type="text" id="search-available" placeholder="Search for a product...">
-                        <div id="selectedCheckbox">
-                            <label for="showSelected" id="showSelected-label">Show selected products</label>
+        <div id="after-dates" class="col ">  
+            <div class="row">
+                <div class="col-lg-4 categories-col">
+                    @include('components.categories')
+                </div>
+                <div class="col">
+                    <div class="row" id="miniSearch">
+                        <div class="col">
+                            <input type="text" id="search-available" placeholder="Search for a product...">
+                        </div>
+                        <div id="selectedCheckbox" class="col-offset-1 col-3 text-nowrap text-right ">
+                            <label for="showSelected" id="showSelected-label">Show selected </label>
                             <input type="checkbox" name="showSelected" id="showSelected" checked="checked">
                         </div>
-                        <ul id="products-list" class="list-group list-group-flush"> 
-                        </ul>
-                    </div>
+                    </div>         
+                    <div class="row">
+                        <div class="col">
+
+                            <ul id="products-list" class="list-group list-group-flush"> 
+                            </ul>
+                        </div>
+                    </div>           
+                </div>
+            </div>
+
             <input id="products" name="products" type="hidden">
         </div>
     </div>
